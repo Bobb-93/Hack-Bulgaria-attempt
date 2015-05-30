dc9f015cbb5801117703fc829d23e6a44f666488
Bobb-93 <borisiakimov@gmail.com>
2015-05-31 02:55:04 +0300

    First two tasks

---
+++ SecondLargest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace SecondLargest
+{
+    class Program
+    {
+        static void second_largest(int[] arr)
+        {
+            int? largest=null;
+            int? secondLargest=null;
+            bool noSecondLargest=false;
+
+            if(arr.Length<2)
+            {
+                noSecondLargest = true;
+            }
+            else if(arr[0]>arr[1])
+            {
+                largest = arr[0];
+                secondLargest = arr[1];
+            }
+            else if(arr[0]<arr[1])
+            {
+                largest = arr[1];
+                secondLargest = arr[0];
+            }
+
+            for(int i=2;i<arr.Length;i++) 
+            {
+                if (arr[i] > secondLargest)
+                {
+                    if (arr[i] > largest)
+                    {
+                        secondLargest = largest;
+                        largest = arr[i];
+                    }
+                    else
+                    {
+                        secondLargest = arr[i];
+                    }
+                }
+            }
+
+            if(secondLargest==null)
+            {
+                noSecondLargest = true;
+            }
+            if (noSecondLargest == true)
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine(secondLargest);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] ourArray = new int[n];
+            Console.WriteLine("Enter the first array:");
+            for (int i = 0; i < n; i++)
+            {
+                ourArray[i] = int.Parse(Console.ReadLine());
+            }
+            second_largest(ourArray);
+            
+        }
+    }
+}
---
+++ profit/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static int profit_and_loss(int[] arr1,int[] arr2)
+    {
+        int income = 0;
+        int outcome = 0;
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            income += arr1[i];
+        }
+
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            outcome += arr2[i];
+        }
+
+        return income-outcome;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the length of the first array: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] profitArray=new int[n];
+        Console.WriteLine("Enter the first array:");
+        for(int i = 0;i<n;i++)
+        {
+	        profitArray[i]= int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Enter the length of the second array: ");
+        int k = int.Parse(Console.ReadLine());
+        int[] lossArray=new int[3];
+        Console.WriteLine("Enter the second array:");
+        for(int i = 0;i<k;i++)
+        {
+	        lossArray[i]= int.Parse(Console.ReadLine());
+        }
+
+        int result=profit_and_loss(profitArray, lossArray);
+        Console.WriteLine(result);
+    }
+}
+
