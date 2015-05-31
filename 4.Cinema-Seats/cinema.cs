c42a7de52475667b0d90e7f2707f122bf9a5966a
Bobb-93 <borisiakimov@gmail.com>
2015-05-31 20:47:37 +0300

    Attempt for 4

--- 4.Cinema-Seats/cinema.cs
+++ 4.Cinema-Seats/cinema.cs
@@ -11,17 +11,41 @@ namespace _4.Cinema_Seats
         //function that finds the row with the least zeroes 
         static void leastZeroes(int[,] ourMatrix)
         {
+            int largestSumOfOnes = 0;
+            int[] sumOfIntsPerRow = new int[20];
+            int sum = 0;
             for(int row=0; row<ourMatrix.GetLength(0);row++)
             {
                 for (int col = 0; col < ourMatrix.GetLength(1);col++)
                 {
-
+                    sum = ourMatrix[row, col];
                 }
             }
-
+            Console.WriteLine(sum);
         }
         static void Main(string[] args)
         {
+            // Read the matrix dimensions
+            Console.Write("Number of rows = ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Number of columns = ");
+            int cols = int.Parse(Console.ReadLine());
+
+            // Allocate the matrix
+            int[,] matrix = new int[rows, cols];
+
+            // Enter the matrix elements
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write("matrix[{0},{1}] = ", row, col);
+                    int element = int.Parse(Console.ReadLine());
+                    matrix[row, col] = element;
+                }
+            }
+
+            leastZeroes(matrix);
         }
     }
 }
